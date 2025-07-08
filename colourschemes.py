7af4f67d95d7a5caf6ac32cb08d06d09aae12502
Tom Kimber <tom.kimber@gmail.com>
2025-07-08 21:26:11 +0100

    added visualisation code, colourschemes and some sample data.

---
+++ colourschemes.py
@@ -0,0 +1,64 @@
+from itertools import cycle
+
+colour_schemes = {"rich10" : [  "#001219", "#005f73", 
+                                "#0a9396", "#94d2bd", 
+                                "#e9d8a6", "#ee9b00", 
+                                "#ca6702", "#bb3e03", 
+                                "#ae2012", "#9b2226"],
+                "spectral10" : ["#ff0000", "#ff8700", 
+                                "#ffd300", "#deff0a", 
+                                "#a1ff0a", "#0aff99", 
+                                "#0aefff", "#147df5", 
+                                "#580aff", "#be0aff"],
+                "muted_rainbow10" : ["#fe4a49","#fe9158",
+                                "#f4b701", "#828646",
+                                "#3c905f", "#008da3",
+                                "#236186", "#462255",
+                                "#734785", "#9d4366"],
+                "metro9" :  [   "#ea5545", "#f46a9b", 
+                                "#ef9b20", "#edbf33", 
+                                "#ede15b", "#bdcf32", 
+                                "#87bc45", "#27aeef",
+                                "#b33dc6"],
+                "dutch9" :  [   "#e60049", "#0bb4ff", 
+                                "#50e991", "#e6d800", 
+                                "#9b19f5", "#ffa300", 
+                                "#dc0ab4", "#b3d4ff", 
+                                "#00bfa0"],
+                "nights9" :    ["#b30000", "#7c1158", 
+                                "#4421af", "#1a53ff",
+                                "#0d88e6", "#00b7c7", 
+                                "#5ad45a", "#8be04e", 
+                                "#ebdc78"],
+                "pastels9" : [  "#fd7f6f", "#7eb0d5", 
+                                "#b2e061", "#bd7ebe", 
+                                "#ffb55a", "#ffee65", 
+                                "#beb9db", "#fdcce5", 
+                                "#8bd3c7"],
+                "bold4" :  ["#ff3e3a", "#0496ff","#ffbc42","#17a600"]
+                            }
+
+def gen_cycle(colour_scheme):
+    """
+    Generate a cycle of colours from the specified colour scheme.
+    
+    :param colour_scheme: Name of the colour scheme to use.
+    :return: A cycle object that can be used to iterate through the colours.
+    """
+    if colour_scheme in colour_schemes:
+        return cycle(colour_schemes[colour_scheme])
+    else:
+        raise ValueError(f"Colour scheme '{colour_scheme}' not found.")
+    
+def get_colour_scheme(colour_scheme):
+    """
+    Get the colour scheme as a list of colours.
+    
+    :param colour_scheme: Name of the colour scheme to retrieve.
+    :return: A list of colours from the specified colour scheme.
+    """
+    if colour_scheme in colour_schemes:
+        return colour_schemes[colour_scheme]
+    else:
+        raise ValueError(f"Colour scheme '{colour_scheme}' not found.")
+
---
+++ graphvisutils_gravis.py
@@ -0,0 +1,209 @@
+"""A collection of generic graph visualisation utilities 
+   intended to work with gravis and the gravis JSON Graph Format gJGF"""
+# https://robert-haas.github.io/gravis-docs/rst/format_specification.html
+from rdflib import Graph, Namespace, URIRef, BNode, Literal
+from rdflib.namespace import RDF, RDFS, OWL, SH
+import networkx as nx
+import uuid
+from itertools import cycle
+import colourschemes
+import numpy as np
+
+def capture_entity_data(rdflib_graph, entity):
+    """For a provided rdflib graph and an identified entity, extract
+    all types, labels, literals and participating properties"""
+    entity_types=None
+    entity_labels=None
+    entity_literals=dict()
+    outbound_entity_properties=dict()
+    inbound_entity_properties=dict()
+
+    for s,p,o in rdflib_graph.triples((entity, RDF.type, None)):
+        if entity_types is not None:
+            entity_types.add(o)
+        else:
+            entity_types=set([o])
+
+    for s,p,o in rdflib_graph.triples((entity, RDFS.label, None)):
+        if entity_labels is not None:
+            entity_labels.add(o)
+        else:
+            entity_labels=set([o])
+        
+
+    for s,p,o in rdflib_graph.triples((entity, None, None)):
+        if isinstance(o, Literal):
+            if p not in entity_literals:
+                entity_literals[p]=set([o])
+            else:
+                entity_literals[p].add(o)
+        else:
+            if p in outbound_entity_properties:
+                outbound_entity_properties[p].add(o)
+            else:
+                outbound_entity_properties[p]=set([o])
+    for s,p,o in rdflib_graph.triples((None, None, entity)):
+        if p in inbound_entity_properties:
+            inbound_entity_properties[p].add(s)
+        else:
+            inbound_entity_properties[p]=set([s])
+            
+    return { "node" : entity, 
+            "types" : entity_types, 
+            "labels" : entity_labels, 
+            "literals" : entity_literals, 
+            "outbound_links" : outbound_entity_properties, 
+            "inbound_links" : inbound_entity_properties }
+
+
+
+def construct_property_table(entity_data):
+    rows=[]
+    panel_html_template="""
+    <h2>Node: {title}</h2>
+    <p>Type: {etype} </br>
+    <p>Label: {elabel} </p>
+    {table}
+    """
+    table_html_template="""
+    <table width="95%">
+        <colgroup>
+            <col span="1" style="width: 70%;">
+            <col span="1" style="width: 30%;">
+        </colgroup>
+    <thead><tr><th width="20%">Property</th><th width="80%">Value</th></tr></thead>
+    <tbody>
+    {rows}
+    </tbody>
+    </table>
+    """
+    row_html_template="""<tr><th>{k}</th><td>{v}</td></tr>"""
+    for k,v in entity_data['literals'].items():
+        rows.append(row_html_template.format(k=k,v="/".join([str(v.value) for v in v])))
+
+    title = entity_data['node']
+    etypes = entity_data['types']
+    if etypes is None:
+        etypes=[]
+    elabels = entity_data.get('labels')
+    if elabels is None:
+        elabels=[]
+    table_html = table_html_template.format(rows="".join(rows))
+    panel_html = panel_html_template.format(
+                                            title = title,
+                                            etype = "/".join([t for t in etypes]),
+                                            elabel = "/".join([t.value for t in elabels]), 
+                                            table=table_html
+                                            )
+
+    return panel_html
+
+
+def rdflib_graph_to_networkx_for_gravis(rdflib_graph, 
+                                        hide_types=False, 
+                                        hide_literals=False, 
+                                        hide_labels=True,
+                                        set_random_node_styles=True,
+                                        node_colour_scheme=None, 
+                                        edge_colour_scheme=None):
+    """Construct a networkx graph with annotations matching those used by the gravis visualisation module.
+    hide_types controls whether type-like nodes are shown as nodes, or just used as styling cues
+    hide_literals controls whether all literal values are projected into the graph, or are attached to nodes property-graph style
+    """
+    nx_g = nx.MultiDiGraph()
+    n_set, p_set = set(), set()
+    types = dict()
+    object_types = dict()
+    literals = set()
+    entity_data_d=dict()
+    for s,p,o in rdflib_graph.triples((None, None, None)):
+        n_set.add(s)
+        p_set.add(p)
+        n_set.add(o)
+        if p in (RDF.type):
+
+            if s not in entity_data_d:
+                entity_data_d[s] = capture_entity_data(rdflib_graph, s)
+
+            if o in types:
+                types[o].add(s)
+            else:
+                types[o]=set([s])
+                
+        if isinstance(o, Literal):
+            literals.add(o)
+    # Define colour cycles for nodes and edges
+    if node_colour_scheme is None:  
+        node_colour_scheme = "spectral10"
+    if edge_colour_scheme is None:
+        edge_colour_scheme = "muted_rainbow10"
+    
+    n_c_cycle = colourschemes.gen_cycle(node_colour_scheme)
+    e_c_cycle = colourschemes.gen_cycle(edge_colour_scheme)    
+    n_s_cycle = cycle(['circle', 'rectangle', 'hexagon'])
+
+    # Cycle over Typed Nodes (Entities)
+    for t,nodes in types.items():
+        tcolor=next(n_c_cycle)
+        t_shape = next(n_s_cycle)
+        if hide_types and t in (RDF.type):
+            # Do not include node in diagram
+            pass
+        else:
+            for n in nodes:
+                # Get Label for Node
+                labels = entity_data_d.get(n).get('labels')
+                if labels is None:
+                    labels=[n.n3(rdflib_graph.namespace_manager)]
+                html_packet = construct_property_table(entity_data_d[n])
+                nx_g.add_node(n, label = "/".join(labels), shape=t_shape, color=tcolor, size=10, click=html_packet)
+    # Remaining Nodes are either Literals, or are Untyped Object Nodes
+        # Cycle over Literals First.
+    for l in literals:
+        if not hide_literals:
+            label = l.value
+            nx_g.add_node(l, label = label, shape='circle', color='gray')
+
+    # Cycle over Edges
+    for p in p_set:
+        p_colour = next(e_c_cycle)
+        for s,pp,o in rdflib_graph.triples((None, p, None)):
+            if hide_types and o in types:
+            # Do not add this link as it ends in a type
+                pass
+            elif hide_literals and o in literals:
+                pass
+            elif hide_labels and p in (RDFS.label):
+                # No need to show labels as separate nodes
+                pass
+            else:
+                labels = entity_data_d.get(pp,{}).get('labels')
+                if labels is None:
+                    labels=[pp.n3(rdflib_graph.namespace_manager)]
+                nx_g.add_edge(s,o,label="/".join(labels), color=p_colour)
+    return nx_g
+
+def rdflib_graph_from_dataframe(dataframe, data_namespace="http://data#"):
+
+    DATA = Namespace(data_namespace)
+    g = Graph(bind_namespaces="rdflib")
+    g.bind("data", DATA)
+    g.add ((DATA.row, RDF.type, OWL.Class))
+    g.add ((DATA.row, RDFS.label, Literal("Row")))
+
+    for c in dataframe.columns:
+        g.add(( DATA[f"column({c})"], RDF.type, OWL.DatatypeProperty )) # Define the column as a datatype property
+        g.add(( DATA[f"column({c})"], RDFS.label, Literal(c) )) # Attach a simple label to the datatype property
+        g.add((DATA.row, DATA.has_field, DATA[f"column({c})"]))
+
+    for row_i,data in dataframe.replace(np.nan, None).iterrows():
+        row_url = DATA[uuid.uuid4().hex]
+        row_index = Literal(row_i)
+        g.add((row_url, RDF.type, DATA.row))
+        g.add((row_url, DATA.row_ident, row_index))
+        for c in dataframe.columns:
+            p_url = DATA[f"column({c})"]
+            if data[c] is not None:
+                o_literal = Literal(data[c])
+            g.add((row_url, p_url, o_literal))
+    return g
